edd822dca0380cba88e7977e73bbc2b9ee355f2a
ivan <ivan@ivanplex.com>
2017-04-25 20:18:06 +0100

    Server & Listener is only runnable if executed directly

--- cast_server.py
+++ cast_server.py
@@ -1,50 +1,50 @@
-#####
-# Network Modules
-from network.multicastServer import MulticastServer
-'''
-Cast multicast traffic from multicast group 
-224.1.1.1 on port 5007
-'''
-multicastServer = MulticastServer('224.1.1.1', 5007)
-
-#####
-# Audio Modules
-from audio.audioHandler import AudioHandler
-audioHandler = AudioHandler()
 import wave
-
-#####
-# Forward Error Correction Module
+from audio.audioHandler import AudioHandler
+from network.multicastServer import MulticastServer
 from fec.fecHandler import FECHandler
-fec = FECHandler()
-
-
-############
-###  Streaming wav File
-wf = wave.open('bin/kygo.wav', 'rb')
-print("recording...")
-#####
 
 
 
-try:
+def cast():
+    """
+    Begin multicasting wav file
+    """
 
-    ### Streaming wav
-    streamdata = wf.readframes(audioHandler.getChunk())
-    
-    while len(streamdata) >0:
-    
-        encoded_bytes = fec.fec_encode(streamdata)
-        multicastServer.send(encoded_bytes)
+    # Streaming wav File
+    wf = wave.open('bin/kygo.wav', 'rb')
+    print("recording...")
 
-        # FEC Debug
-        # print("Raw data: "+ str(len(streamdata)))
-        # print("Encoded: "+ str(len(encoded_bytes)))
+    try:
 
+        ### Streaming wav
         streamdata = wf.readframes(audioHandler.getChunk())
-       
+        
+        while len(streamdata) >0:
+        
+            encoded_bytes = fec.fec_encode(streamdata)
+            multicastServer.send(encoded_bytes)
 
-finally:
-    print('closing socket')
-    multicastServer.terminate()
-    audioHandler.terminate()
+            # FEC Debug
+            # print("Raw data: "+ str(len(streamdata)))
+            # print("Encoded: "+ str(len(encoded_bytes)))
+
+            streamdata = wf.readframes(audioHandler.getChunk())
+           
+
+    finally:
+        print('closing socket')
+        multicastServer.terminate()
+        audioHandler.terminate()
+
+
+if __name__ == "__main__":
+
+    '''
+    Cast multicast traffic from multicast group 
+    224.1.1.1 on port 5007
+    '''
+    multicastServer = MulticastServer('224.1.1.1', 5007)
+    audioHandler = AudioHandler()
+    fec = FECHandler()
+
+    cast()
--- receiver.py
+++ receiver.py
@@ -1,52 +1,49 @@
 from threading import Thread
 from queue import Queue
 
-#####
-# Network Modules
 from network.multicastClient import MulticastClient
-
-'''
-Accept multicast traffic from multicast group 
-224.1.1.1 on port 5007
-'''
-multicastClient = MulticastClient('224.1.1.1', 5007)
-
-#####
-# Audio Modules
 from audio.audioHandler import AudioHandler
-audioHandler = AudioHandler()
-
-#####
-# Forward Error Correction Module
 from fec.fecHandler import FECHandler
-fec = FECHandler()
 
 
 
-frameQueue = Queue()
-frameblock = []
+def listen():
+    frameQueue = Queue()
+    frameblock = []
 
-# Start processing frames when they come available
-audioThread = Thread( target=audioHandler.playAudio, args=[frameQueue] )
-audioThread.start()
+    # Start processing frames when they come available
+    audioThread = Thread( target=audioHandler.playAudio, args=[frameQueue] )
+    audioThread.start()
 
-try:
-    while True:
-        encodedFrame, address = multicastClient.receive(1024)
+    try:
+        while True:
+            encodedFrame, address = multicastClient.receive(1024)
 
-        # Frames need to be individually decoded
-        try:
-            frameblock.append(fec.fec_decode(encodedFrame))
-        except: 
-            print(">>> Broken data, ignoring...")
-            pass
+            # Frames need to be individually decoded
+            try:
+                frameblock.append(fec.fec_decode(encodedFrame))
+            except: 
+                print(">>> Broken data, ignoring...")
+                pass
 
-        if(len(frameblock)%100) == 0:
-            #Put the chunk of packages into a queue
-            frameQueue.put(frameblock)
-            frameblock = []
+            if(len(frameblock)%100) == 0:
+                #Put the chunk of packages into a queue
+                frameQueue.put(frameblock)
+                frameblock = []
 
-finally:
-    print('closing socket')
-    multicastClient.terminate()
-    audioHandler.terminate()
+    finally:
+        print('closing socket')
+        multicastClient.terminate()
+        audioHandler.terminate()
+
+
+if __name__ == "__main__":
+    '''
+    Accept multicast traffic from multicast group 
+    224.1.1.1 on port 5007
+    '''
+    multicastClient = MulticastClient('224.1.1.1', 5007)
+    audioHandler = AudioHandler()
+    fec = FECHandler()
+
+    listen()
