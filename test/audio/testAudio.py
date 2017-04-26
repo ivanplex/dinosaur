41eb598c4e11b3ebee42b395df97b85c7329520d
ivan <ivan@ivanplex.com>
2017-04-26 16:50:04 +0100

    Update .travis.yml

--- test/audio/testAudio.py
+++ test/audio/testAudio.py
@@ -3,3 +3,9 @@ from audio.audioHandler import AudioHandler
 def test_Object_Creation():
 	audioHandler = AudioHandler()
 	assert audioHandler != None
+
+def test_stream_creation():
+	audioHandler = AudioHandler()
+	assert audioHandler.getStream() != None
+
+
